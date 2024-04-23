9fdf76ce6af7deeb025d695194b4e2e2a5e38711
JuanValeraDev <juanvaleradev@gmail.com>
2024-04-23 08:14:34 +0200

    Lo he seguido intentando un poco pero sigue sin funcionar.

--- client.js
+++ client.js
@@ -4,7 +4,6 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 
-// Set the drawing size of the canvas to match the size of the canvas element in the DOM
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
@@ -36,7 +35,7 @@ canvas.addEventListener("mousemove", (e) => {
 canvas.addEventListener("mouseup", () => {
     isDrawing = false;
 });
-let clientId = null; // Add a variable to store the client's ID
+let clientId = null;
 
 ws.onopen = function () {
     ws.send(JSON.stringify({request: 'clientId'}));
@@ -46,25 +45,25 @@ ws.onmessage = function (event) {
     const data = JSON.parse(event.data);
 
     if (data.type === 'init') {
-        // Initialize the active squares with the data from the server
         activeSquares = data.squares;
         if (activeSquares[clientId]) {
             color = activeSquares[clientId].color;
         }
-        drawSquares();
+        drawSquares().then(r => drawLines());
     } else if (data.type === 'move') {
         activeSquares[data.id] = {x: data.x, y: data.y, color: data.color};
-        drawSquares();
+        drawSquares().then(r => drawLines());
+
     } else if (data.type === 'remove') {
-        // Remove the square from the active squares
         delete activeSquares[data.id];
-        drawSquares();
-    } else   if (data.type === 'draw') {
+        drawSquares().then(r => drawLines());
+
+    } else if (data.type === 'draw') {
         drawLine(context, lastX, lastY, data.x, data.y, data.color);
         [lastX, lastY] = [data.x, data.y];
 
-        // Add the line to the lines array
         lines.push({x1: lastX, y1: lastY, x2: data.x, y2: data.y, color: data.color});
+        drawLines()
     }
 
 };
@@ -79,18 +78,13 @@ function drawLine(context, x1, y1, x2, y2, color) {
     context.closePath();
 }
 
-function drawSquares() {
-    // Clear the entire canvas
-    //context.clearRect(0, 0, canvas.width, canvas.height);
+async function drawSquares() {
+    await context.clearRect(0, 0, canvas.width, canvas.height);
 
-
-    // Draw the squares
     for (let id in activeSquares) {
-        console.log("Drawing square")
         let square = activeSquares[id];
         let centerX = square.x - 75;
         let centerY = square.y - 50;
-        // Convert the color to RGBA for transparency
         let color = square.color.replace('#', '');
         let r = parseInt(color.substring(0, 2), 16);
         let g = parseInt(color.substring(2, 4), 16);
@@ -103,9 +97,11 @@ function drawSquares() {
         context.fillStyle = "black";
         context.fillText("Usuario " + id, centerX + 50, centerY + 50);
     }
-    // Draw the lines
+    drawLines()
+}
+
+function drawLines() {
     for (let line of lines) {
-        console.log("Drawing line")
         drawLine(context, line.x1, line.y1, line.x2, line.y2, line.color);
     }
 }
